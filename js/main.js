3f1d7bc503be606dc5b8854fea0886831d467281
Annie Juraski <anniejuraski@gmail.com>
2026-05-19 18:41:33 -0300

    removed js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    new Swiper('.swiper', {
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
-});
